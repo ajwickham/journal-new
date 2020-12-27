b106b1f1e2f9c882040ced84b9f08825266e2829
Alan Wickham <ajwickham@yahoo.com>
2020-12-27 16:18:14 +0000

    First commit of working code with changes to add import and export and split js files

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+import Entry from './Entry.js';
+
+    // UI or front end logic
+
+$(document).ready(function() {
+  let i = -1
+  document.getElementById("title").value = "";
+  document.getElementById("body").value = "";
+  //  attachContactListeners();
+
+  $("form#new-entry").submit(function(event) {
+    
+    // event.preventDefault();
+    let newTitle = $("input#title").val();
+    let newBody = $("textarea#body").val();
+    game1.addEntry(newTitle,newBody);
+    document.getElementById("title").value = "";
+    document.getElementById("body").value = "";
+    event.preventDefault();
+    
+  });
+  
+  $("button#first").click(function() {
+    i=i+1;
+    if(i>(game1.titles.length-1)) {
+      i=0
+    }  
+    document.getElementById("title").value = game1.titles[i];
+    document.getElementById("body").value = game1.body[i];   
+  });
+  
+  $("button#last").click(function() {
+    i=i-1;
+    if(i<0) {
+      i=(game1.titles.length-1)
+    }
+    document.getElementById("title").value = game1.titles[i];
+    document.getElementById("body").value = game1.body[i];     
+  });
+
+  $("button#new").click(function() {
+    document.getElementById("title").value = "";
+    document.getElementById("body").value = "";  
+  });   
+
+  $("button#delete").click(function() {
+    game1.deleteEntry(i);
+    document.getElementById("title").value = game1.titles[(i)];
+    document.getElementById("body").value = game1.body[(i)];
+  });   
+
+
+});
+
+
+  
